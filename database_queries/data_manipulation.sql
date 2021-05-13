4c66a7964decbd94c1baefb210c97f4b0de15c19
Lindsay Goins <lindsayggoins@gmail.com>
2021-05-12 23:32:45 -0600

    Added INSERT, UPDATE, and DELETE queries

--- database_queries/data_manipulation.sql
+++ database_queries/data_manipulation.sql
@@ -1,5 +1,43 @@
+-- : character represents user input
+
+--Show Users
 SELECT * FROM Users;
 
+--Show Comments
 SELECT * FROM Comments;
 
+--Show relationship between Subreddits & Users
 SELECT * FROM Subreddits_Users;
+
+--Add a new User
+INSERT INTO Users (username, karma, cakeDay)
+VALUES (:usernameInput, :karmaInput, :cakeDayInput);
+
+--Add a new Comment
+INSERT INTO Comments (body, numUpvotes, commentDate, postID, userID)
+VALUES (:bodyInput, :numUpvotesInput, :commentDateInput, :postIDInput, :userIDInput);
+
+--Add a relationship between Subreddits & Users
+INSERT INTO Subreddits_Users (subredditID, userID)
+VALUES (:subredditIDInput, :userIDInput);
+
+--Filter Comments by Username, 'i_love_dachshunds93' as the username will give results
+SELECT Comments.body, Users.username FROM Comments
+INNER JOIN Users ON Comments.userID=Users.userID
+WHERE Users.username = :username;
+
+--Edit (Update) a Comment
+UPDATE Comments SET body = :newComment
+WHERE Comments.commentID = :commentID;
+
+--Delete a Comment
+DELETE FROM Comments
+WHERE Comments.commentID = :commentID;
+
+--Delete a User
+DELETE FROM Users
+WHERE Users.userID = :userID;
+
+--Removes the relationship between a Subreddit and a User
+DELETE FROM Subreddits_Users
+WHERE Subreddits_Users.subredditUserID = :subredditUserID;
